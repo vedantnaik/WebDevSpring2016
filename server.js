67860395840ecbcd7ee33f946d7572e0da7b8959
Vedant Naik <naik.v@husky.neu.edu>
2016-02-16 23:35:08 -0500

    try #2

--- server.js
+++ server.js
@@ -3,7 +3,4 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/', function(req, res){
-    res.sendfile('index.html', { root: "." } );
-});
 app.listen(port, ipaddress);
