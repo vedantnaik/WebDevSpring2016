c23ca6b5fc16c85e9f4ae99001ea7fd4bbc1c042
Vedant Naik <naik.v@husky.neu.edu>
2016-03-20 21:48:57 -0400

    trying fix for 503 error on openshift

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var multer = require('multer');
 var cookieParser = require('cookie-parser');
 var public_folder = __dirname + '/public';
 var app = express();
+var uuid=require('node-uuid');
 
 app.use(multer());
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.get('/assignment', function(req, res){
     res.sendfile('/assignment/client/index.html');
 });
 
-require('./public/assignment/server/app.js')(app);
+require('./public/assignment/server/app.js')(app, uuid);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
