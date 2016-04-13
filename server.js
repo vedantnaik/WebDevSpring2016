0425fbe5131dd72a3ba8edfc0b6a5b366728d511
Vedant Naik <naik.v@husky.neu.edu>
2016-04-12 23:29:09 -0400

    trying to fix openshift startup error

--- server.js
+++ server.js
@@ -11,15 +11,13 @@ var passport = require('passport');
 var session = require('express-session');
 
 var dbName = 'assignment';
-var connectionString = 'mongodb://localhost/' + dbName;
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    //connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    //        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    //        process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
-    //        process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
-    //        process.env.OPENSHIFT_APP_NAME;
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
+var connectionString = 'mongodb://localhost/'+dbName;
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
 }
 
 var db = mongoose.connect(connectionString);
@@ -37,6 +35,10 @@ app.use(session({
 
 app.get('/assignment', function(req, res){
     res.sendfile('/assignment/client/index.html');
+});
+
+app.get('/project', function(req, res){
+    res.sendfile('/project/client/index.html');
 });
 
 require('./public/assignment/server/app.js')(app, db, mongoose);
