ca5cb4ae66ae82a49d847574ca3dff186cc7efcf
Vedant Naik <naik.v@husky.neu.edu>
2016-04-12 23:46:32 -0400

    trying to fix openshift startup error 2

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 
-var dbName = 'assignment';
-var connectionString = 'mongodb://localhost/'+dbName;
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
-
-var db = mongoose.connect(connectionString);
-
 app.use(multer());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,6 +20,22 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+
+var connectionString = 'mongodb://localhost/assignment';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString =
+        process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html', { root: __dirname } );
+});
 
 app.get('/assignment', function(req, res){
     res.sendfile('/assignment/client/index.html');
@@ -47,9 +51,5 @@ require('./public/experiments/projectPrototype/server/app.js')(app, db, mongoose
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.get('/', function(req, res){
-    res.sendfile('index.html', { root: __dirname } );
-});
 
 app.listen(port, ipaddress);
