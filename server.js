27fefa9d0903c4d0369d6044ee3e2c9478bb7e58
Vedant Naik <naik.v@husky.neu.edu>
2016-04-12 23:21:22 -0400

    trying to fix openshift startup error

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ var dbName = 'assignment';
 var connectionString = 'mongodb://localhost/' + dbName;
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
-            process.env.OPENSHIFT_APP_NAME;
+    //connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //        process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+    //        process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
+    //        process.env.OPENSHIFT_APP_NAME;
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
 }
 
 var db = mongoose.connect(connectionString);
