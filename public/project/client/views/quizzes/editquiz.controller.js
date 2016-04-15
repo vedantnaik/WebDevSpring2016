2e6fe761c98a2fc377b46619384014109aa76f12
Vedant Naik <naik.v@husky.neu.edu>
2016-04-15 00:11:38 -0400

    edit quiz functionality complete

--- public/project/client/views/quizzes/editquiz.controller.js
+++ public/project/client/views/quizzes/editquiz.controller.js
@@ -22,6 +22,7 @@
 
         vm.deleteAllQuestions = deleteAllQuestions;
         vm.publishQuiz = publishQuiz;
+        vm.unpublishQuiz = unpublishQuiz;
 
         vm.message = null;
 
@@ -33,6 +34,8 @@
 
         function init(){
 
+            var oldVmMessage = vm.message;
+
             vm.quizToEditId = $routeParams.quizId;
 
             QuizService
@@ -40,38 +43,56 @@
                 .then(
                     function(res){
                         vm.quizToEdit = res.data;
+
+                        QuestionService
+                            .findQuestionsInQuizById(vm.quizToEditId)
+                            .then(
+                                function(res){
+                                    vm.questionsInThisQuiz = res.data;
+
+                                    UserService
+                                        .getCurrentUser()
+                                        .then(
+                                            function (res) {
+                                                var userFromServer = res.data;
+                                                vm.userEditingTheQuiz = userFromServer;
+                                                FactService.findAllFactsForUser(vm.userEditingTheQuiz._id)
+                                                    .then(
+                                                        function( res ){
+
+                                                            var storedData = res.data;
+                                                            var driverStoredResult = storedData
+                                                                .filter(function (rec) {return rec.factType === 'DRR';});
+                                                            var constructorStoredResult = storedData
+                                                                .filter(function (rec) {return rec.factType === 'CRR';});
+
+                                                            if(vm.championshipType === 'Drivers Championship') {
+                                                                vm.standingsSearchTypeDriver = true;
+                                                                vm.displayStoredResults = null;
+                                                                vm.displayStoredResults = driverStoredResult;
+                                                            } else {
+                                                                vm.standingsSearchTypeDriver = false;
+                                                                vm.displayStoredResults = null;
+                                                                vm.displayStoredResults = constructorStoredResult;
+                                                            }
+                                                        },
+                                                        function (err)  {
+                                                            console.log("UNABLE TO GET STORED FACTS TO CLIENT SIDE");
+                                                        }
+                                                    );
+
+                                            },
+                                            function (err) {
+
+                                            }
+                                        );
+                                }
+                            );
                     },
                     function(err){
                         vm.message = "Sorry, we could not find the quiz you are looking for."
                     }
                 );
-
-            console.log("Search for questions under " + vm.quizToEditId);
-            QuestionService
-                .findQuestionsInQuizById(vm.quizToEditId)
-                .then(
-                    function(res){
-                        vm.questionsInThisQuiz = res.data;
-                        console.log(vm.questionsInThisQuiz);
-                    }
-                );
-
-            UserService
-                .getCurrentUser()
-                .then(
-                    function (res) {
-                        var userFromServer = res.data;
-                        vm.userEditingTheQuiz = userFromServer;
-                        displayResults();
-                    },
-                    function (err) {
-
-                    }
-                );
-
-
-
-
         }
 
 
@@ -160,6 +181,31 @@
         function deleteAllQuestions(listOfQuestions){
             vm.message = null;
 
+            QuizService
+                .getQuizById(vm.quizToEditId)
+                .then(
+                    function (res) {
+                        var quizToEmpty = res.data;
+                        quizToEmpty.questions = [];
+
+                        QuizService
+                            .updateQuizById(vm.quizToEditId, quizToEmpty)
+                            .then(
+                                function(res){
+                                    vm.message = "Questions deleted from quiz. Your quiz will no longer be published";
+                                    unpublishQuiz();
+                                },
+                                function(err){
+                                    vm.message = "Caution: Questions may not have been deleted properly";
+                                }
+                            );
+                    },
+                    function (err) {
+                        vm.message = "Caution: Questions may not have been deleted properly";
+                    }
+                );
+
+
             for(var questionIndex in listOfQuestions){
 
                 console.log(listOfQuestions[questionIndex]._id);
@@ -168,7 +214,6 @@
                     .deleteQuestionById(listOfQuestions[questionIndex]._id)
                     .then(
                         function(res){
-                            console.log("Deleted");
                             init();
                         },function(err){
                             vm.message = "There was some error deleting questions from this quiz.";
@@ -177,12 +222,79 @@
             }
 
             console.log("Deleted for complete");
-            init();
         }
 
         function publishQuiz(){
             vm.message = null;
 
+            QuizService
+                .getQuizById(vm.quizToEditId)
+                .then(
+                    function (res) {
+                        var quizToPublish = res.data;
+
+                        if(quizToPublish.questions.length < 1){
+                            quizToPublish.publishedStatus = "NOT PUBLISHED";
+                            //vm.message = "Please add some questions to your quiz before publishing.";
+
+                            QuizService                     // update quiz as unpublished
+                                .updateQuizById(vm.quizToEditId, quizToPublish)
+                                .then(
+                                    function (unpublishedRes) {
+                                        init();
+                                        //"Your quiz is no longer published. Add questions to publish a quiz.";
+                                    },
+                                    function (err) {
+                                        vm.message = "Unable to update the status of your quiz. Please try again.";
+                                    }
+                                );
+
+                        } else {
+                            quizToPublish.publishedStatus = "PUBLISHED";
+
+                            QuizService
+                                .updateQuizById(vm.quizToEditId, quizToPublish)
+                                .then(
+                                    function (publishedRes) {
+                                        vm.message = "Your quiz is now published! Thank you!";
+                                        init();
+                                    },
+                                    function (err) {
+                                        vm.message = "Unable to update the status of your quiz. Please try again.";
+                                    }
+                                );
+                        }
+                    },
+                    function (err) {
+                        vm.message = "Some thing went wrong. Please try again."
+                    }
+                );
+        }
+
+        function unpublishQuiz() {
+            QuizService
+                .getQuizById(vm.quizToEditId)
+                .then(
+                    function (res) {
+                        var quizToUnpublish = res.data;
+                        quizToUnpublish.publishedStatus = "NOT PUBLISHED";
+
+                        QuizService                     // update quiz as unpublished
+                            .updateQuizById(vm.quizToEditId, quizToUnpublish)
+                            .then(
+                                function (unpublishedRes) {
+                                    init();
+                                    vm.message = "Your quiz is no longer published.";
+                                },
+                                function (err) {
+                                    vm.message = "Unable to update the status of your quiz. Please try again.";
+                                }
+                            );
+                    },
+                    function (err) {
+                        vm.message = "Unable to take down your quiz. It is still live."
+                    }
+                );
         }
     }
 
