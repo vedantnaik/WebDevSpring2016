294d73acdc6fac874cb348f2f5dc11f072491790
Vedant Naik <naik.v@husky.neu.edu>
2016-04-15 22:09:47 -0400

    user can update construtor preference

--- public/project/client/views/users/profile.controller.js
+++ public/project/client/views/users/profile.controller.js
@@ -8,15 +8,17 @@
         .module("F1ExplorerApp")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController(UserService, $rootScope, $scope) {
+    function ProfileController(UserService, ErgastService, $rootScope, $scope) {
 
         var vm  = this;
 
         vm.error = null;
         vm.message = null;
         vm.update = update;
+        vm.selectedConstructor = selectedConstructor;
 
         function init() {
+
             UserService
                 .getCurrentUser()
                 .then(function (res) {
@@ -28,6 +30,28 @@
                     $rootScope.currentUser = userFromServer;
 
                     vm.user = userFromServer;
+                    vm.userChoosesToSupport = vm.user.supportConstructor;
+                    var currentYear = new Date().getFullYear();
+                    vm.constructorOptions = [];
+
+                    ErgastService
+                        .getConstructorStandingForSeasonRound(currentYear, "last")
+                        .then(
+                            function (latestSeasonRes) {
+                                var currentSeasonList = latestSeasonRes.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+                                currentSeasonList
+                                    .forEach(
+                                        function (constr) {
+                                            vm.constructorOptions.push(constr.Constructor.name);
+                                        }
+                                    );
+                            },
+                            function (err){
+                                // couldn't talk to Ergast
+                                vm.constructorOptions = ["Williams", "Haas F1 Team", "Toro Rosso", "Renault", "Mercedes", "Ferrari", "McLaren", "Force India", "Sauber", "Manor Marussia", "Red Bull"]
+                            }
+                        );
+
                 });
         }
         return init();
@@ -38,6 +62,8 @@
             //console.log("UPDATE USER IN CONTROLLER: " + vm.user._id);
             vm.error = null;
             vm.message = null;
+
+            user.supportConstructor = vm.userChoosesToSupport;
 
             UserService
                 .updateUser(vm.user._id, user)
@@ -52,6 +78,10 @@
                             //console.log("Unable to update your profile. Try again!");
                         }
                     });
+        }
+
+        function selectedConstructor(constructor){
+            vm.userChoosesToSupport = constructor;
         }
     }
 
