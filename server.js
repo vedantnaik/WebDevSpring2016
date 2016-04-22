04e88d40250be831b089e96d5c81bdb4fe6da2ae
Vedant Naik <naik.v@husky.neu.edu>
2016-04-22 15:03:44 -0400

    breaking assignment to make project work on openshift

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/project', function(req, res){
     res.sendfile('/project/client/index.html');
 });
 
-require('./public/assignment/server/app.js')(app, db, mongoose);
-//require('./public/project/server/app.js')(app, db, mongoose);
+//require('./public/assignment/server/app.js')(app, db, mongoose);
+require('./public/project/server/app.js')(app, db, mongoose);
 require('./public/experiments/projectPrototype/server/app.js')(app, db, mongoose);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
